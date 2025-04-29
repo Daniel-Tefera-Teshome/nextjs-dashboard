763c75efdbc8f9b4c0180a47dd8ce2ca8501a1fd
Dani <danieltefera14@gmail.com>
2025-04-29 16:42:02 +0300

    form valiation and auth are added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Dani Dashboard",
+    default: "Dani Dashboard",
+  },
+  description: "Next.js training Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 export default function RootLayout({
   children,
 }: {
